2a8c015767024dd89978eb7c015f76f760043f97
Winchestor2001 <sbexruz2001@gmail.com>
2024-04-17 23:13:44 +0500

    update folder dir

--- core/settings.py
+++ core/settings.py
@@ -12,11 +12,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import sys
 from environs import Env
 from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(os.path.join(BASE_DIR, "apps"))
 env = Env()
 env.read_env()
 
