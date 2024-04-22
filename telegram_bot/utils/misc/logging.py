5f638b38c77a42e9eb31984d1bdff45363927e8f
Winchestor2001 <sbexruz2001@gmail.com>
2024-04-22 12:59:55 +0500

    update settings

--- telegram_bot/utils/misc/logging.py
+++ telegram_bot/utils/misc/logging.py
@@ -1,10 +1,6 @@
 import logging
-from environs import Env
 
-env = Env()
-env.read_env()
-
-logging.basicConfig(filename=env.str('BOT_LOG_PATH'),format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
+logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO,
                     # level=logging.DEBUG,  # Можно заменить на другой уровень логгирования.
                     )
